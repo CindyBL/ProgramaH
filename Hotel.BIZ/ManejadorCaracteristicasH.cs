588478aea982951749a15c1edbbfd552cd061940
Cinndy <Cinndy@DESKTOP-H5LJDRT>
2020-05-25 16:03:08 -0500

    h

---
+++ Hotel.BIZ/ManejadorCaracteristicasH.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.BIZ
+{
+    class ManejadorCaracteristicasH
+    {
+        //IRepositorio<CaracteristicasHabitacion> repositorio;
+
+        //public ManejadorCaracteristicasH(IRepositorio<CaracteristicasHabitacion> repo)
+        //{
+        //    repositorio = repo;
+        //}
+
+        //public List<CaracteristicasHabitacion> Listar => repositorio.Read;
+
+        //public bool Agregar(CaracteristicasHabitacion entidad)
+        //{
+        //    return repositorio.Create(entidad);
+        //}
+
+        //public CaracteristicasHabitacion BuscarPorId(ObjectId id)
+        //{
+        //    return Listar.Where(e => e.Id == id).SingleOrDefault();
+        //}
+
+        //public bool Eliminar(ObjectId id)
+        //{
+        //    return repositorio.Delete(id);
+        //}
+
+        //public bool Modificar(CaracteristicasHabitacion entidad)
+        //{
+        //    return repositorio.Update(entidad);
+        //}
+    }
+}
